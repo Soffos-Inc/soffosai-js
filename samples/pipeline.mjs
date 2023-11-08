cf057daf472505ac6af793eee101cd1f341f0456
francosuch <franco.such@soffos.ai>
2023-11-08 09:30:17 +0800

    bugfix: sample pipeline.mjs

--- samples/pipeline.mjs
+++ samples/pipeline.mjs
@@ -1,43 +1,50 @@
-import { SoffosPipelines } from "soffosai";
-import { SoffosNodes } from "soffosai";
+import { SoffosPipeline, SoffosAIServices } from "soffosai";
 
 /**
  * The document search service provides "passages" which is a list of contents plus some more description.
  * In order to get the content, iterate through the passages and concatenate it.
+ * @private
  * @param {Array.<object>} value 
  * @returns {string}
  */
-function getContent(value) {
+function getContent(passages) {
     let combined_text = "";
-    for (let item of value) {
-        combined_text += item.content;
-    }
+        for (let passage of passages) {
+            combined_text += passage.content;
+        }
     return combined_text
 }
 
-export class AskADocumentPipeline extends SoffosPipelines.SoffosPipeline {
+/**
+ * When you already have a document uploaded to Soffos, use its document_id and ask questions about the doc.
+ * @class
+ * @alias _SoffosPipelines.AskADocumentPipeline
+ */
+class AskADocumentPipeline extends SoffosPipeline {
     /**
      * @param {string} name - The name of this pipeline. Will be used to reference this pipeline
      *  if this pipeline is used as a Node inside another pipeline.
      * @param {Object} kwargs - Include other needed properties like apiKey
-     * @returns {AskADocumentPipeline}
      */
     constructor(name=null, kwargs={}){
-        let d_node = new SoffosNodes.DocumentsSearchNode(
-                "search", null, null, {source: "user_input", field: "doc_ids"}
-            );
-            let qa_node = new SoffosNodes.QuestionAnsweringNode(
+        let d_node = new SoffosAIServices.DocumentsSearchService(kwargs);
+        d_node.setInputConfigs(
+                "search", null, null, createInputConfig("user_input", "doc_ids")
+        );
+        
+        let qa_node = new SoffosAIServices.QuestionAnsweringService(kwargs);
+        qa_node.setInputConfigs(
                 "qa",
-                {source: "user_input", field: "question"},
-                {source: "search", field: "passages", pre_process: getContent}
-            );
+                createInputConfig("user_input", "question"),
+                createInputConfig("search", "passages", getContent)
+        );
                 
-        let nodes = [d_node, qa_node];
-        return super(nodes, false, name, kwargs);
+        let services = [d_node, qa_node];
+        return super(services, false, name, kwargs);
     }
 
     /**
-     * Call the pipeline
+     * Start the pipeline processes.
      * @param {string} user - The ID of the user accessing the Soffos API.  Soffos assumes that the owner of
      * the api is an application (app) and that app has users. Soffos API will accept any string.
      * @param {Array.<string>} doc_ids - the document IDs of included document.
@@ -45,6 +52,20 @@ export class AskADocumentPipeline extends SoffosPipelines.SoffosPipeline {
      * @param {string} [execution_code=null] - If this process should be tracked so it can be
      * terminated via terminate() method, execution_code should be provided to reference this pipeline call.
      * @returns {Object}
+     * {<br>
+     *  search: {
+     *      passages: "List of passages",
+     *  }, <br>
+     *  qa: {
+     *      answer: "The answer to the query."
+     *  }<br>
+     * } 
+     * @example
+     * let pipe = new AskADocumentPipeline("my_pipeline", {apiKey: my_apiKey});
+     * // On this test, the API key used must have access to document "1d77babf8164427cad8276ba944e6cbc"
+     * // Please ingest a document first and replace the document_id here.
+     * let result = await pipe.call("client_id", ["1d77babf8164427cad8276ba944e6cbc"], "Who is Neo?");
+     * console.log(JSON.stringify(result, null, 2)); 
      */
     async call(user, doc_ids, question, execution_code=null) {
         let payload = {
@@ -55,25 +76,7 @@ export class AskADocumentPipeline extends SoffosPipelines.SoffosPipeline {
         }
         return await this.run(payload);
     }
+
 }
 
-const my_apiKey = "Token <put your api key here>";
-// Check if apiKey has been updated, just in case the user forgot before executing this sample:
-if (my_apiKey=="Token <put your api key here>") {
-    throw Error("Please update the API Key value assigned to my_apiKey");
-}
-
-// Instantiate the pipeline
-let pipe = new AskADocumentPipeline("my_pipeline", {apiKey: my_apiKey});
-
-// On this test, the API key used must have access to document "1d77babf8164427cad8276ba944e6cbc"
-// Please ingest a document first and replace the document_id here.
-let result = await pipe.call("client_id", ["1d77babf8164427cad8276ba944e6cbc"], "Who is Neo?");
-console.log(JSON.stringify(result, null, 2));
-
-
-/////////////////////////////////////////////////////////////////////////////////////////////////
-// you can also import from pre-defined pipelines:
-import { SoffosPipelines } from "soffosai";
-
-let pipe2 = new SoffosPipelines.FileIngestPipeline({apiKey: my_apiKey});
+export default AskADocumentPipeline
