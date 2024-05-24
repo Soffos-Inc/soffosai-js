ebf209f91e5b1f6930eb97588f878371d7f24bb8
Waleed Umer <wumer@siammakro.co.th>
2024-05-24 13:47:05 +0500

    fix: request for microlessons

--- soffosai/src/core/services/microlesson.mjs
+++ soffosai/src/core/services/microlesson.mjs
@@ -32,25 +32,6 @@ class MicrolessonService extends SoffosAIService {
    *     "client1234567",
    *     [
    *         {
-   *             "source": "Telescope.docx",
-   *             "text": "The James Webb Space Telescope is the largest, most powerful \
-   *             space telescope ever built. It will allow scientists to look at what our \
-   *             universe was like about 200 million years after the Big Bang. The telescope \
-   *             will be able to capture images of some of the first galaxies ever formed. \
-   *             It will also be able to observe objects in our solar system from Mars outward, \
-   *             look inside dust clouds to see where new stars and planets are forming and \
-   *             examine the atmospheres of planets orbiting other stars."
-   *         },
-   *         {
-   *             "source": "dogs.docx",
-   *             "text": "Genetic evidence suggests that dogs descended directly from wolves (Canis) \
-   *             and that the now-extinct wolf lineages that produced dogs branched off from the \
-   *             line that produced modern living wolves sometime between 27,000 and 40,000 years ago. \
-   *             The timing and location of dog domestication is a matter of debate. \
-   *             There is strong genetic evidence, however, that the first domestication events \
-   *             occurred somewhere in northern Eurasia between 14,000 and 29,000 years ago.",
-   *         },
-   *         {
    *             "source": "Text",
    *             "text": "Rehabilitation for Patellofemoral Syndrome 'Chondromalacia patella \'
    *             Brett Sanders, MD Center For Sports Medicine and Orthopaedic 2415 McCallie Ave. \
